5b64744e3df1a21e0e423ef4054bdf3a64670f67
Carl Tidelius <carl_tidelius@live.se>
2018-02-13 16:13:59 +0100

    perceptron start

--- LinearRegression.m
+++ LinearRegression.m
@@ -19,9 +19,24 @@ y_fr = FR(:, 2)./max_a_count;
 
 x_eng = ENG(:,1)./max_char_count;
 y_eng = ENG(:, 2)./max_a_count;
+eng = '0';
+fr = '1';
 
-W_fr = BatchGradientDecent([x_fr y_fr], 0.01);
-W_eng = BatchGradientDecent([x_eng y_eng], 0.01);
+eng = strcat('0', ':', num2str(x_eng(:,1)), {' '});
+yeng = strcat('1', ':', num2str(y_eng(:,1)), {' '});
+eng = [eng yeng];
+eng = [eng{:}];
+eng = regexprep(eng, ':\s+', ':');
+eng = ['0 ' eng]
+fr = strcat('0', ':', num2str(x_fr(:,1)), {' '});
+yfr = strcat('1', ':', num2str(y_fr(:,1)), {' '});
+fr = [fr yfr];
+fr = [fr{:}];
+fr = regexprep(fr, ':\s+', ':');
+fr = ['1 ' fr]
+
+W_fr = StochasticGradientDescent([x_fr y_fr], 0.01);
+W_eng = StochasticGradientDescent([x_eng y_eng], 0.01);
 
 x = linspace(0, 1, 100);
 y_grad_fr = polyval(W_fr, x);
--- StochasticGradientDescent.m
+++ StochasticGradientDescent.m
@@ -1,6 +1,5 @@
-function [ output_args ] = StochasticGradientDescent( input_args )
 
-function W = BatchGradientDecent(L, learn_rate)
+function W = StochasticGradientDecent(L, learn_rate)
 convergence_distance = 1E-10;
 
 w0 = 0.5;
@@ -8,14 +7,15 @@ w1 = 0;
 w0_pre = 0;
 w1_pre = 0;
 while 1 == 1
-   randnum = rand(1, 15);
+   randnum = randperm(15, 1);
    x = L(randnum, 1);
    y = L(randnum, 2);
-   
-   w0 = w0 + learn_rate *  (y - (w0 + w1 * x));
-   w1 = w1 + learn_rate * x * (y - (w0 + w1 * x));
-   
-   if ( <= convergence_distance &&  <= convergence_distance)
+   w0_pre = w0;
+   w1_pre = w1;
+   w0 = w0 + learn_rate *  2 * (y - (w0 + w1 * x));
+   w1 = w1 + learn_rate * 2 * x * (y - (w0 + w1 * x));
+
+   if (abs(w0 - w0_pre) <= convergence_distance && abs(w1 - w1_pre) <= convergence_distance)
        W = [w1 w0];
     break;
    end
@@ -25,5 +25,4 @@ end
 
 end
 
-end
 
